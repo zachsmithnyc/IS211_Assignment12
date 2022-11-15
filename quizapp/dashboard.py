6dd98e9632de170bb270891e15b87ba339b72ead
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-11-15 18:40:51 -0500

    Assignment 12

--- quizapp/dashboard.py
+++ quizapp/dashboard.py
@@ -7,3 +7,15 @@ from quizapp.auth import login_required
 from quizapp.db import get_db
 
 bp = Blueprint('dashboard', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    students = db.execute(
+        'SELECT id, name FROM students'
+    ).fetchall()
+    quizzes = db.execute(
+        'SELECT quiz_id, subject, length, date FROM quizzes'
+    ).fetchall()
+    return render_template('dashboard/dashboard.html', students=students, quizzes=quizzes)
+    
