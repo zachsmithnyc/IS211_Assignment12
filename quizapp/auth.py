f97689c1122936fdb115a11ddd966b0c0a7fe24f
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-11-15 18:07:01 -0500

    assignment 12

--- quizapp/auth.py
+++ quizapp/auth.py
@@ -9,3 +9,92 @@ from quizapp.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    ''' 
+    Registers a new User.
+    Validates that username is not taken.
+    hashes password. 
+    '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+        flash(error)
+    
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    '''Log in a registered user by adding their id to the session'''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('dashboard'))
+
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    '''if a use id is stored in the session
+    load the user object from the database into g.user
+    '''
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db.execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    '''clear the current session, including stored user id'''
+    session.clear()
+    return redirect(url_for('dashboard'))
+
+def login_required(view):
+    '''view decorator that redirects anonymous users to the login page'''
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+    
+    return wrapped_view
