f7570d1ca171ea1802de872876b83a70daca7ef8
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-11-15 19:13:05 -0500

    assignment 12

--- quizapp/auth.py
+++ quizapp/auth.py
@@ -78,7 +78,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db.execute(
+        g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
