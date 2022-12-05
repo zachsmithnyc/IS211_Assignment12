5dbf6cb2ba91fe4b5db43620a0bfebcd3c0f0816
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-12-05 12:44:41 -0500

    Assignment 12

--- quizapp/__init__.py
+++ quizapp/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it esits, when not testing
+        # load the instance config, if it exits, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
